1b1bada178d42b5c7164344a3efbd7d92ed3cab7
Joonatan Palmu <jjmpal@utu.fi>
2018-12-17 10:59:28 +0200

    Packrat

---
+++ init.R
@@ -0,0 +1,18 @@
+local({
+    r <- getOption("repos")
+    r["CRAN"] <- "https://cran.uib.no/"
+    options(repos = r)
+})
+
+if (FALSE) {
+    install.packages("packrat")
+    packrat::init("~/microbiome/articletwo")
+    packrat::canUseGitHubDownloader()
+}
+
+getOption("repos")
+install.packages("BH")
+install.packages("dplyr")
+
+install.packages("ggplot2")
+install.packages("tidyr")
