77032774f1d8e54bd59a393feac448b179593dc6
Joonatan Palmu <jjmpal@utu.fi>
2019-01-01 15:50:42 +0200

    Adonis, bin

--- bin/reports.sh
+++ bin/reports.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd ${HOME}/phd/research/articletwo/ || exit 1
+cd "${HOME}/phd/research/articletwo/reports" || exit 1
+HOSTPATH="utu:articletwo"
+FILE="rrbiome.html"
+BACKUP="rrbiome_$(date +%Y%m%d).html"
 
-rsync -avu utu:articletwo/*.{html,pdf} reports/ #&& open reports/rrbiome.html
+RET=$(rsync -aEim $HOSTPATH/$FILE $FILE)
+
+if [ $? -eq 0 -a -n "${RET}" ]; then
+	cp $FILE $BACKUP
+	open $FILE
+fi
