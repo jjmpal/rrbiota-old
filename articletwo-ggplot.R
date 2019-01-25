7705e432da0d8bcc5bcef0bf722a219461c5655a
Joonatan Palmu <jjmpal@utu.fi>
2019-01-25 12:47:28 +0200

    New file

---
+++ articletwo-ggplot.R
@@ -0,0 +1,78 @@
+plot.alpha <- function(diversity,  colorlimit = 0.25) {
+    plot.alpha.diversity <- ggplot(diversity, aes(x=reorder(Name, R2),  y = 1, fill = estimate)) +
+        geom_bar(stat="identity", color="black") +
+        coord_flip() +
+        scale_fill_gradientn(colours = c("darkblue", "white", "darkred"),
+                             values = rescale(c(-colorlimit, 0, colorlimit)),
+                             name = 'Regression coefficient \nin linear model \nfor Shannon index',
+                             limits = c(-colorlimit, colorlimit),
+                             na.value = "black") +
+        theme_classic(20) +
+        geom_point(aes(Name, y=0.5, shape=Qstar), show.legend=TRUE, color='black', size=20) +
+        scale_shape_manual(name="",
+                           values=c('*'='*', ' '=' '),
+                           labels=c("*"="significant at FDR 0.05", ' '=' '),
+                           breaks=c("*", ' ')) +
+        xlab("") +
+        ylab("") +
+        theme(legend.position="right",
+              legend.title=element_text(size = 14),
+              legend.direction="vertical",
+              legend.text=element_text(size = floor(0.75*20)),
+              legend.box.margin=margin(t = 0, unit = "cm"),
+              legend.margin=margin(0,0,0,0,"pt"),
+              legend.justification="left",
+              axis.line.y = element_line(linetype="blank"),
+              axis.line.x = element_line(linetype="blank"),
+              legend.title.align = 0,
+              legend.text.align = 0,
+              axis.text.y = element_text(color="black"),
+              axis.text.x = element_text(color="black"))+
+        scale_y_continuous(breaks=c(0.5), labels=c(expression(alpha)))
+
+    plot.alpha.diversity.gtable <- ggplot_gtable(ggplot_build(plot.alpha.diversity))
+    plot.alpha.diversity.legend <- legend_extractor(plot.alpha.diversity.gtable)
+    plot.alpha.diversity.yaxis <- plot.alpha.diversity.gtable$grobs[[3]]
+
+    plot.alpha.diversity <- plot.alpha.diversity +
+        theme(legend.position = "none",
+              axis.text.x = element_blank(),
+              axis.text.y=element_blank())
+
+    plot.gtable.alpha.diversity.stripped <- ggplot_gtable(ggplot_build(plot.alpha.diversity))
+    return(list(plot = plot.gtable.alpha.diversity.stripped$grobs[[6]],
+                yaxis = plot.alpha.diversity.yaxis,
+                xaxis = plot.alpha.diversity.gtable$grobs[[7]],
+                legend = plot.alpha.diversity.legend))
+}
+
+plot.beta <- function(diversity, fig.permar2.text = "R2 of variable \nin Bray-Curtis distance", fig.textsize = 15) {
+    plot.beta.diversity <- ggplot(diversity, aes(x = reorder(Name, R2), y= R2)) +
+        geom_bar(stat="identity", color="black") +
+        coord_flip() +
+        theme_classic(fig.textsize) +
+        ylab(fig.permar2.text) +
+        theme(axis.text.y = element_text(color="black"),
+              axis.text.x = element_text(color="black"),
+              legend.position="right",
+              legend.direction="vertical",
+              legend.box.margin=margin(0,0,0,0,"pt"),
+              legend.margin=margin(0,0,0,0,"pt"),
+              legend.justification="left",
+              legend.title.align = 0,
+              legend.text.align = 0)
+
+    plot.gtable.beta.diversity <- ggplot_gtable(ggplot_build(plot.beta.diversity))
+    plot.xlab.beta.diversity <- xlabb_extractor(plot.gtable.beta.diversity)
+
+    plot.beta.diversity <- plot.beta.diversity  +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_blank(),
+              axis.title.x  =  element_blank(),
+              axis.title.y = element_blank(),
+              legend.position = "none")
+    plot.gtable.beta.diversity.stripped <- ggplot_gtable(ggplot_build(plot.beta.diversity))
+
+      return(list(plot =plot.gtable.beta.diversity.stripped$grobs[[6]],
+                xaxis = plot.gtable.beta.diversity$grobs[[7]]))
+}
