861b24d152bd8b91e4fb9d4047a5f8d78b4e98f8
Joonatan Palmu <jjmpal@utu.fi>
2019-08-26 13:38:59 +0300

    Sensitivity analysis

--- .Rprofile
+++ .Rprofile
@@ -2,6 +2,22 @@ local({r <- getOption("repos")
        r["CRAN"] <- "http://cran.r-project.org" 
        options(repos=r)
 })
+
+myinstall.packages <- function(...) {
+    if("gtools" %in% rownames(installed.packages()) == FALSE) {
+        install.packages("gtools")
+    }
+    list.of.packages <- c(...)
+    new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+    if (length(new.packages) == 0) { return(TRUE) }
+    for (package in new.packages) {
+        message(sprintf("Installing: %s", package))
+        myinstall.packages(gtools::getDependencies(package))
+        install.packages(package)
+    }
+}
+
+
 #### -- Packrat Autoloader (version 0.5.0) -- ####
 source("packrat/init.R")
 #### -- End Packrat Autoloader -- ####
--- articletwo-officer.R
+++ articletwo-officer.R
@@ -66,7 +66,7 @@ tableone <- function(dset) {
         flextable::align(align = "left", part = "body", j = 1)
 }
 
-typologyformatter <- function(data, font = 12, typology) {
+typologyformatter <- function(data, font = 12, typology, left = c(1), hleft = c(1)) {
   flex <- flextable(data = data) %>%
     flextable::theme_booktabs() %>%
     flextable::border(border = fp_border(width=0), part="body") %>%
@@ -74,21 +74,24 @@ typologyformatter <- function(data, font = 12, typology) {
     flextable::border(part="header", border.bottom = fp_border(width=1))
 
   if (!missing(typology)) {
-    flex <- flex %>%
-      set_header_df(mapping = typology, key = "col_keys") %>%
-      merge_h(part = "header") %>%
-      flextable::border(part="header", border.bottom = fp_border(width=1))
+      flex <- flex %>%
+          set_header_df(mapping = typology, key = "col_keys") %>%
+          merge_h(part = "header") %>%
+          flextable::border(part="header", border.bottom = fp_border(width=1))
+      if (missing(hleft)) {
+          hleft <- c(2)
+      }
   }
 
   flex %>%
-    flextable::border(i = nrow(data), part="body", border.bottom = fp_border(width=1)) %>%
-    flextable::bold(bold = FALSE, part = "header") %>%
-    flextable::bold(bold = FALSE, part = "body") %>%
-    flextable::fontsize(size = font, part = "header") %>%
-    flextable::fontsize(size = font, part = "body") %>%
-    flextable::align(align = "center", part = "all") %>%
-    flextable::align(align = "left", part = "header", j = 1) %>%
-    flextable::align(align = "left", part = "body", j = 1)
+      flextable::border(i = nrow(data), part="body", border.bottom = fp_border(width=1)) %>%
+      flextable::bold(bold = FALSE, part = "header") %>%
+      flextable::bold(bold = FALSE, part = "body") %>%
+      flextable::fontsize(size = font, part = "header") %>%
+      flextable::fontsize(size = font, part = "body") %>%
+      flextable::align(align = "center", part = "all") %>%
+      flextable::align(align = "left", part = "header", j = left, i = hleft) %>%
+      flextable::align(align = "left", part = "body", j = left)
 }
 
 xtableformatter <- function(table, font = 12) {
