7638f7c7b2702361e8751b8de5015c7fac7731bb
Joonatan Palmu <jjmpal@utu.fi>
2019-12-20 08:34:09 +0200

    Init

---
+++ articletwo-ggplot.R
@@ -0,0 +1,222 @@
+plot.alpha <- function(diversity,
+                       alphabreaks = c(-Inf, -1, -0.1, -0.01, 0)) {
+    diversity <- diversity %>%
+        mutate(estimate = ifelse(response == "HYPERTENSION", log(alpha.effect), alpha.effect),
+               estimate_fac =  cut(estimate, breaks = alphabreaks),
+               Qstar = ifelse(alpha.p < 0.05, '*', ' '))
+
+    plot.alpha.diversity <- ggplot(diversity, aes(x = reorder(Name, deseqplotorder(Name)),
+                                                  y = 1,
+                                                  fill = estimate_fac)) +
+        geom_bar(stat="identity", color="black") +
+        coord_flip() +
+        scale_fill_brewer(palette="Blues",
+                          name = 'Regression coefficient \nin linear model \nfor Shannon index',
+                          drop=FALSE,
+                          direction = -1) +
+        theme_classic(20) +
+        geom_point(aes(Name, y=0.5, shape=Qstar), show.legend=FALSE, color='black', size=14) +
+        scale_shape_manual(name="",
+                           values=c('*'='*', ' '=' '),
+                           labels=c("*"="significant at\nFDR 0.05", ' '=' '),
+                           breaks=c("*", ' ')) +
+        xlab("") +
+        ylab("") +
+        theme(legend.position="right",
+              legend.title=element_text(size = 14),
+              legend.direction="vertical",
+              legend.text=element_text(size = floor(0.75*20)),
+              legend.box.margin=margin(t = 0, unit = "cm"),
+              legend.margin=margin(0,0,0,0,"pt"),
+              legend.justification="left",
+              axis.line.y = element_line(linetype="blank"),
+              axis.line.x = element_line(linetype="blank"),
+              legend.title.align = 0,
+              legend.text.align = 0,
+              axis.text.y = element_text(color="black"),
+              axis.text.x = element_text(color="black"))+
+        scale_y_continuous(breaks=c(0.5), labels=c(expression(alpha)))
+
+    plot.alpha.diversity.gtable <- ggplot_gtable(ggplot_build(plot.alpha.diversity))
+    plot.alpha.diversity.legend <- legend_extractor(plot.alpha.diversity.gtable)
+    plot.alpha.diversity.yaxis <- plot.alpha.diversity.gtable$grobs[[3]]
+
+    plot.alpha.diversity <- plot.alpha.diversity +
+        theme(legend.position = "none",
+              axis.text.x = element_blank(),
+              axis.text.y=element_blank())
+
+    plot.gtable.alpha.diversity.stripped <- ggplot_gtable(ggplot_build(plot.alpha.diversity))
+    return(list(plot = plot.gtable.alpha.diversity.stripped$grobs[[6]],
+                yaxis = plot.alpha.diversity.yaxis,
+                xaxis = plot.alpha.diversity.gtable$grobs[[7]],
+                legend = plot.alpha.diversity.legend))
+}
+
+plot.beta <- function(diversity,
+                      fig.textsize = 15,
+                      ymax = 0.0016) {
+    diversity <- diversity %>% mutate(Qstar = ifelse(beta.p < 0.05, "*", "-"))
+    plot.beta.diversity <- ggplot(diversity,
+                                  aes(x = reorder(Name, deseqplotorder(Name)), y = beta.R2)) +
+        geom_bar(stat="identity", color = "black", fill = "gray96") +
+        geom_point(aes(y = beta.R2 - 0.00007, shape=Qstar), show.legend=FALSE, color='black', size=14) +
+        scale_shape_manual(values=c('*'='*', '-'='')) +
+        coord_flip() +
+        theme_classic(fig.textsize) +
+        ylab("Coefficient of determination for\nbeta diversity") +
+        scale_y_continuous(limits = c(0, ymax)) +
+        theme(legend.position="right",
+              legend.direction="vertical",
+              legend.box.margin=margin(0,0,0,0,"pt"),
+              legend.margin=margin(0,0,0,0,"pt"),
+              legend.justification="left",
+              legend.title.align = 0,
+              legend.text.align = 0)
+
+    plot.gtable.beta.diversity <- ggplot_gtable(ggplot_build(plot.beta.diversity))
+    plot.xlab.beta.diversity <- xlabb_extractor(plot.gtable.beta.diversity)
+
+    plot.beta.diversity <- plot.beta.diversity  +
+        theme(axis.text.x = element_blank(),
+              axis.text.y = element_blank(),
+              axis.title.x  =  element_blank(),
+              axis.title.y = element_blank(),
+              legend.position = "none")
+    
+    plot.gtable.beta.diversity.stripped <- ggplot_gtable(ggplot_build(plot.beta.diversity))
+
+    return(list(plot = plot.gtable.beta.diversity.stripped$grobs[[6]],
+                xaxis = plot.gtable.beta.diversity$grobs[[7]],
+                xlab = plot.xlab.beta.diversity))
+}
+
+myforestplot <- function(data,
+                         xlab = "",
+                         ylab = "",
+                         interceptone = FALSE,
+                         subtitle = "",
+                         my_y_scale = scale_y_continuous()) {
+    ggplot(data = data,
+           aes(x = term, y = estimate, ymin = conf.low, ymax = conf.high)) +
+        geom_point() +
+        { if (interceptone) geom_hline(aes(fill=fake_salt), yintercept=1, linetype=2) } +
+        geom_errorbar(width=0.1)+ 
+        ggplot2::theme_minimal()  +
+        theme(plot.title=element_text(size=16),
+              axis.title=element_text(size=10),
+              strip.background = element_blank()) +
+        theme(panel.border = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank(),
+              axis.ticks.x = element_line(),
+              axis.ticks.y = element_line(),
+              axis.line = element_line(colour = "black")) +
+        scale_x_discrete(labels=seq(3)) +
+        my_y_scale + 
+        xlab(xlab) +
+        ylab(ylab) +
+        labs(subtitle = subtitle)
+}
+
+
+gvlma.table.plot <- function(model) {
+    { sink("/dev/null");
+        gvlma.model <- gvlma::gvlma(model)
+        gvlma.modek.summary <- summary(gvlma.model)
+        ; sink(); }
+    ret <- list("table" = gvlma.modek.summary,
+                 "plot" = gvlma.model)
+    return(ret)
+}
+
+plot.diversities <- function(diversities) {
+    alpha.diversity.min <- plot.alpha(diversities$min, alphabreaks = seq(-1,0,0.2))
+    alpha.diversity.max <- plot.alpha(diversities$max, alphabreaks = seq(-1,0,0.2))
+
+    beta.diversity.min <- plot.beta(diversities$min)
+    beta.diversity.max <- plot.beta(diversities$max)
+    
+    gs4 <- list(alpha.diversity.min$yaxis,
+                alpha.diversity.min$plot,
+                beta.diversity.min$plot,
+                alpha.diversity.min$legend,
+                alpha.diversity.min$xaxis,
+                beta.diversity.min$xaxis,
+                beta.diversity.min$xlab,
+                text_grob("Age- and sex-adjusted model", size = 18),
+                alpha.diversity.max$yaxis,
+                alpha.diversity.max$plot,
+                beta.diversity.max$plot,  
+                alpha.diversity.max$xaxis,
+                beta.diversity.max$xaxis,
+                beta.diversity.max$xlab,
+                text_grob("Multivariable-adjusted model", size = 18))
+ 
+    g4 <- arrangeGrob(grobs = gs4,
+                      layout_matrix = rbind(
+                          c(8,    8,    8, NA,   NA, NA, NA),
+                          c(NA, 1,    2,    NA, 3, NA, NA),
+                          c(NA, 1,    2,    NA, 3, NA, 4),
+                          c(NA, NA, 5,    NA,6, NA, NA),
+                          c(NA, NA, NA, NA,7, NA, NA),
+                          c(15,    15,    15, NA,   NA, NA, NA),
+                          c(NA, 9,    10,  NA,  11, NA, NA),
+                          c(NA, 9,    10,  NA,  11, NA, NA),
+                          c(NA, NA, 12,   NA, 13, NA, NA),
+                          c(NA, NA, NA, NA,14, NA, NA)),
+                      widths=c(0.7, 4, 0.7, 0.1, 5, 0.7, 3),
+                      heights=rep(c(1.5, 1, 6, 1, 2), 2))
+    return(g4)
+}
+
+deseqhaetmap <- function(df, legend.name = "Log Base Two\nFold Change") {
+    ggplot(df,
+           aes(x = reorder(Name, deseqplotorder(Name)),
+               y = ordered(Feature, levels=rev(unique(Feature))),
+               fill = log2FoldChange)) +
+        geom_tile(aes(fill = 1)) +
+        geom_tile(colour="white", size=0.25) +
+        scale_fill_distiller(palette = "RdBu",
+                             name = legend.name,
+                             limits=c(-1, 1),
+                             breaks = c(-1, -0.5, 0.5, 0, 1),
+                             na.value="grey30") +
+        xlab("") +
+        ylab("") +
+        theme_classic() +  
+        theme(axis.text.x = element_text(angle=90, size=10, hjust=1.0,vjust=0.5),
+              axis.text.y = element_text(size=10),
+              legend.title = element_text(size=10),
+              legend.position = "right")
+}
+
+
+deseqplotorder <- function(x) {
+    y <- seq(length(x))
+    y[x == "Systolic BP"] = 0
+    y[x == "Diastolic BP"] = 1
+    y[x == "Pulse pressure"] = 2
+    y[x == "MAP"] = 3
+    y[x == "Hypertension"] = 4
+    y
+}
+
+saltplot <- function(dset, ymax = 2) {
+    ggplot(data = dset, aes(x = NA., y = g_Lactobacillus)) +
+        geom_point(aes(color = SEX), size = 0.05, position = "jitter") +
+        geom_smooth(method='lm', formula= y~x, colour = "black", size = 0.4) +
+        scale_x_continuous(limits=c(20, 205), expand = c(0, 0)) +
+        scale_y_log10(limits = c(100, 10000), expand = c(0, 0)) +
+        scale_colour_manual(name = "SEX",
+                            labels = c("Male", "Female"),
+                            breaks=c("0", "1"),
+                            values = c("blue", "red")) +
+        xlab("24-hour urinary sodium [mmol]") +
+        ylab("Normalized Lactobacillus abundance count") +
+        guides(colour = guide_legend(override.aes = list(size=4, alpha = 0.4))) +
+        theme_classic() +
+        theme(plot.title = element_blank(),
+              legend.title = element_blank(),
+              legend.position = c(0.9,0.95))
+}
---
+++ articletwo-officer.R
@@ -0,0 +1,185 @@
+characteristics <- function(dset, tableone.names, tableone.factors, extras = list()) {
+    title <- "Characteristics"
+    overall <- paste0("Cases, n=", dim(dset)[1])
+    tableobject <- tableone::CreateTableOne(data = dset, vars = names(tableone.names), factorVars = tableone.factors)
+    tablecsv <- print(tableobject,
+                      exact = "stage",
+                      quote = FALSE,
+                      noSpaces = TRUE,
+                      printToggle = FALSE,
+                      digits = 1,
+                      pDigits = 3,
+                      contDigits=1)
+
+    tableone.fullnames <- c(tableone.names, extras)
+    tablecsv %>%
+        as.data.frame %>%
+        tibble::rownames_to_column(var = "rowname") %>%
+        dplyr::filter(row_number() > 1) %>%
+        format(justify = "left", trim = TRUE) %>%
+        rowwise() %>%
+        mutate(id = gsub("^ *([A-Za-z_0-9]+).*", "\\1", rowname)) %>%
+        mutate(present = id %in%  names(tableone.fullnames)) %>%
+        mutate(rowname = ifelse(present == TRUE, tableone.fullnames[[id]], rowname)) %>%
+        select(rowname, Overall)
+}
+
+tableone <- function(dset) {
+
+    tableone.names <- list("BL_AGE" = "Age, y (SD)",
+                           "SEX" = "Female, N (%)",
+                           "BMI" = "BMI, kg/m² (SD)",
+                           "oSYSTM" = "Systolic BP, mmHg (SD)",
+                           "oDIASM" = "Diastolic BP, mmHg (SD)",
+                           "oPULSEPRESSURE" = "Pulse pressure, mmHg (SD)",
+                           "oMAP" = "Mean arterial pressure, mmHg (SD)",
+                           "HYPERTENSION" = "Hypertension, N (%)",
+                           "CURR_SMOKE" = "Current smoker, N (%)",
+                           "PREVAL_DIAB" = "Diabetes mellitus, N (%)",
+                           "Q57X" = "Exercise, N (%)",
+                           "ANYDRUG" = "Antihypertensive medication, N (%)",
+                           "BL_USE_RX_C03" = "  Diuretics, N (%)",
+                           "BL_USE_RX_C07" = "  Beta blockers, N (%)",
+                           "BL_USE_RX_C08" = "  Calcium channel blockers, N (%)",
+                           "BL_USE_RX_C09" = "  RAS blockers, N (%)")
+    extras <- list("1"  =  "  Light",
+                           "2"  =  "  Moderate",
+                           "3"  =  "  Heavy")
+    tableone.factors <- c("SEX", "HYPERTENSION",  "ANYDRUG", "CURR_SMOKE", "PREVAL_DIAB",
+                          "BL_USE_RX_C03", "BL_USE_RX_C07", "BL_USE_RX_C08", "BL_USE_RX_C09")
+    data <- characteristics(dset, tableone.names, tableone.factors, extras)
+
+    flextable(data = data) %>%
+        set_header_labels(rowname = "Characteristics",
+                          Overall = paste0("Cases, n=", dim(dset)[1])) %>%
+        flextable::width(j = 1, width = 3) %>%
+        flextable::width(j = 2, width = 1.2) %>%
+        flextable::border(border = fp_border(width=0), part="body") %>%
+        flextable::border(border = fp_border(width=0), part="header") %>%
+        flextable::border(part="header", border.bottom = fp_border(width=1)) %>%
+        flextable::border(i = nrow(data), part="body", border.bottom = fp_border(width=1)) %>%
+        flextable::bold(bold = FALSE, part = "header") %>%
+        flextable::bold(bold = FALSE, part = "body") %>%
+        flextable::fontsize(size = 12, part = "header") %>%
+        flextable::fontsize(size = 12, part = "body") %>%
+        flextable::align(align = "center", part = "all") %>%
+        flextable::align(align = "left", part = "header", j = 1) %>%
+        flextable::align(align = "left", part = "body", j = 1)
+}
+
+typologyformatter <- function(data, font = 12, typology, left = c(1), hleft = c(1)) {
+  flex <- flextable(data = data) %>%
+    flextable::theme_booktabs() %>%
+    flextable::border(border = fp_border(width=0), part="body") %>%
+    flextable::border(border = fp_border(width=0), part="header") %>%
+    flextable::border(part="header", border.bottom = fp_border(width=1))
+
+  if (!missing(typology)) {
+      flex <- flex %>%
+          set_header_df(mapping = typology, key = "col_keys") %>%
+          merge_h(part = "header") %>%
+          flextable::border(part="header", border.bottom = fp_border(width=1))
+      if (missing(hleft)) {
+          hleft <- c(2)
+      }
+  }
+
+  flex %>%
+      flextable::border(i = nrow(data), part="body", border.bottom = fp_border(width=1)) %>%
+      flextable::bold(bold = FALSE, part = "header") %>%
+      flextable::bold(bold = FALSE, part = "body") %>%
+      flextable::fontsize(size = font, part = "header") %>%
+      flextable::fontsize(size = font, part = "body") %>%
+      flextable::align(align = "center", part = "all") %>%
+      flextable::align(align = "left", part = "header", j = left, i = hleft) %>%
+      flextable::align(align = "left", part = "body", j = left)
+}
+
+xtableformatter <- function(table, font = 12) {
+  xtable_to_flextable(x = xtable(table),
+                      NA.string = "",
+                      include.rownames = FALSE) %>%
+    flextable::border(border.top = fp_border(width=0), part="header", border.bottom = fp_border()) %>%
+    flextable::bold(bold = FALSE, part = "header") %>%
+    flextable::bold(bold = FALSE, part = "body") %>%
+    flextable::fontsize(size = font, part = "header") %>%
+    flextable::fontsize(size = font, part = "body") %>%
+    flextable::align(align = "center", part = "all") %>%
+    flextable::align(align = "left", part = "header", j = 1) %>%
+    flextable::align(align = "left", part = "body", j = 1)
+}
+
+riskmodel.getn <- function(df, riskclass, inviduals = FALSE, cols = c("htn")) {
+  ret <- df %>%
+    group_by(.dots = riskclass) %>%
+    summarize(n=n(), htn=sum(HT == 1)) %>%
+    select(cols)
+  return(rbind(c("", ""), c("", ""), c("", ""), ret))
+}
+
+
+riskmodel.onecolumn <- function(riskmodels) {
+  lapply(riskmodels, function(result) {
+    cbind(
+      rbind(result$unadj_persd$mean_ci,
+            result$unadj_persd$p,
+            "",
+            "1.0 (referent)",
+            cbind(result$unadj_class$mean_ci)),
+      rbind(result$adj_persd$mean_ci,
+            result$adj_persd$p,
+            "",
+            "1.0 (referent)",
+            cbind(result$adj_class$mean_ci)))
+  })
+}
+
+
+riskmodel.joincolumns <- function(rset, riskmodels, idforn = "fwdbonf_riskclass", order = list()) {
+  beginning.list <- cbind(
+    c("Per 1-SD", "p", "Quartile", "   Q1", "   Q2", "   Q3", "   Q4"),
+    riskmodel.getn(rset, idforn, cols = "n")
+  )
+  col.list <- lapply(order, function(ord) {
+    cbind(riskmodel.getn(rset, paste0(ord, "_riskclass")),
+          get(ord, riskmodels))
+  })
+  col.names <- lapply(order, function(ord) {
+    c(paste0(ord, ".event"), paste0(ord, "unadjusted"), paste0(ord, ".adjusted"))
+  })
+  list <- do.call(cbind, list(beginning.list, col.list)[lapply(list(beginning.list, col.list), length)>0])
+  colnames(list) <- c("term", "n", unlist(col.names))
+  list
+}
+
+writetable <- function(doc, tbl, number = 0, head, foot) {
+    text.bold <- fp_text(font.size = 12, bold = TRUE, font.family = "Arial")
+    text.normal <- fp_text(font.size = 12, font.family = "Arial")
+
+    doc <- doc %>%
+        body_add_fpar(fpar(ftext(paste0("Table ", number, ". "), prop = text.bold), ftext(head, prop = text.normal)), style = "Table Caption") %>%
+        body_add_flextable(tbl, align = "left") %>%
+        body_add_par(foot, style = "footnote text") %>%
+        body_add_break(pos = "after")
+}
+
+writeimage <- function(doc, number = 0, filename, header, footer, width = 7) {
+text.bold <- fp_text(font.size = 12, bold = TRUE, font.family = "Arial")
+text.normal <- fp_text(font.size = 12, font.family = "Arial")
+
+  img <- readPNG(filename)
+  doc <- doc %>%
+    body_add_fpar(fpar(ftext(paste0("Figure ", number, ". "), prop = text.bold), ftext(header, prop = text.normal)), style = "Image Caption") %>%
+    body_add_img(src = filename, width = width, height = width*dim(img)[1]/dim(img)[2]) %>%
+    body_add_par(footer, style = "footnote text") %>%
+    body_add_break(pos = "after")
+}
+
+myspread <- function(ret, list = c2l("lfc_se", "p.value"), term  = "Feature", key = "Name") {
+    lapply(list, function(x) ret %>%
+                             select(term, key, x) %>%
+                             spread(key, x) %>%
+                             rename_at(vars(-term), funs(paste0(., "_", x)))) %>%
+        Reduce(function(...) full_join(..., by = term), .) %>%
+        dplyr::select(term, noquote(order(colnames(.))))
+}
